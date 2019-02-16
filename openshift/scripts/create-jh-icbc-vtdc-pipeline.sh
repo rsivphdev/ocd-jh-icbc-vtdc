0eab1008e916c0e74d899178e5614250a7e2be46
David Kerins <dkerins@bitbox.ca>
2019-02-16 09:06:02 -0800

    Working template examples

--- openshift/scripts/create-jh-icbc-vtdc-pipeline.sh
+++ openshift/scripts/create-jh-icbc-vtdc-pipeline.sh
@@ -3,7 +3,7 @@
 git clone --single-branch --branch pipeline-enabled https://github.com/rsivphdev/pocicbcvtdc.git
 sleep 3
 oc login -u cluster-admin
-oc new-app -n jik2hd-tools -f jh-icbc-vtdc-template.yaml
+oc new-app -n jik2hd-tools -f ../templates/jh-icbc-vtdc-template.yaml
 oc create configmap jhicbcvtdc-config -n jik2hd-dev --from-file=pocicbcvtdc/modules/pssg/poc/icbc/main/module.xml --from-file=pocicbcvtdc/modules/pssg/poc/icbc/main/vphsc.properties
 oc create configmap jhicbcvtdc-config -n jik2hd-test --from-file=pocicbcvtdc/modules/pssg/poc/icbc/main/module.xml --from-file=pocicbcvtdc/modules/pssg/poc/icbc/main/vphsc.properties
 sleep 3
@@ -18,3 +18,4 @@ echo "Check for TEST Deployment Configuration (dc) \n"
 oc get dc
 sleep 3
 oc set volume dc/jhicbcvtdc --overwrite --add --name=jhicbcvtdc-mnt --mount-path=/wildfly/provided_modules/pssg/poc/icbc/main --type=configmap --configmap-name=jhicbcvtdc-config
+rm -rf ./pocicbcvtdc
